c322b8cd783d77b2e19ef2e29be3a372ba5d042c
Armando Castany Marconi <castanymarconiaj1@cardiff.ac.uk>
2018-10-15 17:02:20 +0100

    Added new templates <p and <P for org-babel

---
+++ lisp/init-templates.el
@@ -0,0 +1,17 @@
+;;This file contains some of my easy templates for better org & babel
+
+;;;CODE:
+
+;; This adds :results, session, noweb and tangle.
+
+(add-to-list 'org-structure-template-alist
+             '("P" "#+BEGIN_SRC python :results output :noweb :tangle :session?\n\n#+END_SRC" "<src lang=\"?\">\n\n</src>"))
+
+;; This adds just the results output.
+
+(add-to-list 'org-structure-template-alist
+             '("p" "#+BEGIN_SRC python :results output :noweb :tangle?\n\n#+END_SRC" "<src lang=\"?\">\n\n</src>"))
+
+(provide 'init-templates)
+
+;;init-templates ends here.
